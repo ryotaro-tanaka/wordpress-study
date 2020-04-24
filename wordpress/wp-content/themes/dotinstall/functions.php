e5d6058ddb72326de9e97f90bce8b421960d0f4c
ryotaro tanaka <ryotaro_tanaka@outlook.com>
2020-04-24 09:13:24 +0900

    fix sidebar.php

--- wordpress/wp-content/themes/dotinstall/functions.php
+++ wordpress/wp-content/themes/dotinstall/functions.php
@@ -2,3 +2,11 @@
 
 add_theme_support('menus'); 
 
+register_sidebar(
+    array(
+        'before_widget' => '<div class="widget">',
+        'after_widget' => '</div>',
+        'before_title' => '<h3>',
+        'after_title' => '</h3>',
+    )
+);
